ed0fa38e528dc83a6ad63e094b217a743ce44a28
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2025-03-26 18:31:26 -0700

    Don't re-render the whole app

--- src/main.js
+++ src/main.js
@@ -4,15 +4,28 @@ import './style.css'
 function main() {
   const item = data[Math.floor(Math.random() * data.length)];
   const { section, chapter } = item;
-  document.querySelector('#app').innerHTML = `
-    <div>
+
+  const app = document.querySelector('#app');
+  const contentWrapper = document.querySelector('#content-wrapper');
+
+  if (contentWrapper) {
+    contentWrapper.outerHTML = renderContent(chapter, section);
+  } else {
+    app.innerHTML = `
+      ${renderContent(chapter, section)}
+      <button id="reroll">Reroll</button>
+    `;
+    document.querySelector('#reroll').addEventListener('click', main);
+  }
+}
+
+function renderContent(chapter, section) {
+  return `
+    <div id="content-wrapper">
       <h1>${chapter}</h1>
       <h2>${section}</h2>
-      <button id="reroll">Reroll</button>
     </div>
   `;
-  document.querySelector('#reroll').addEventListener('click', main)
-
 }
 
 main();
